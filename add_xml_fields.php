6c5069f84372b45afc3a04ffd8ade44574e83604
Uziel Almeida Oliveira <uzielweb@gmail.com>
2017-10-10 03:48:30 -0300

    Removed bad foreach with explode
    Changed 
    if ($option == $theadmincomponent)
    to
    if (in_array($option, $theadmincomponents))
    
    Changed 
    if ($option == $thesitecomponent)
    to
    if (in_array($option, $thesitecomponents))
    
    Changed 
    if ($module == $themodule)
    to
    if (in_array($module, $themodules))
    
    Now is possible to add multiple xml file names and your respectives components or modules names correctly.

--- add_xml_fields.php
+++ add_xml_fields.php
@@ -1,242 +1,152 @@
 <?php
-
 // no direct access
-
-defined ('_JEXEC') or die;
-
-class plgSystemAdd_xml_fields extends JPlugin
-
-{
-
+defined('_JEXEC') or die;
+class plgSystemAdd_xml_fields extends JPlugin {
 /**
+
+
 
 * Load the language file on instantiation.
 
+
+
 * Note this is only available in Joomla 3.1 and higher.
+
+
 
 * If you want to support 3.0 series you must override the constructor
 
+
+
 *
+
+
 
 * @var boolean
 
+
+
 * @since <your version>
 
+
+
 */
-
-protected $autoloadLanguage = true;
-
+  protected $autoloadLanguage = true;
 /**
+
+
 
 * Prepare form and add my field.
 
+
+
 *
+
+
 
 * @param   JForm  $form  The form to be altered.
 
+
+
 * @param   mixed  $data  The associated data for the form.
 
+
+
 *
+
+
 
 * @return  boolean
 
+
+
 *
+
+
 
 * @since   <your version>
 
+
+
 */
-
-function onContentPrepareForm($form, $data)
-
-{
-
-
-
-
-
-
-
-$app = JFactory::getApplication();
-
-$option = $app->input->get('option');
-
-$theadmincomponentsfiles    = explode(",", $this->params->get('theadmincomponentsfiles'));
-
-$theadmincomponents = explode(",", $this->params->get('theadmincomponents'));
-
-$thesitecomponentsfiles     = explode(",", $this->params->get('thesitecomponentsfiles'));
-
-$thesitecomponents = explode(",", $this->params->get('thesitecomponents'));
-
-$pieces     = explode(",", $this->params->get('filenames'));
-
-$themodules = explode(",", $this->params->get('themodules'));
-
-
-
-
-
-
-
-$db    = JFactory::getDBO();
-
-
-
-
-
-
-
-$query = 'SELECT module'
-
-.' FROM #__modules'
-
-.' WHERE id = ' . $db->Quote($app->input->get("id"));
-
-$db->setQuery($query);
-
-$module = $db->loadResult();
-
+  function onContentPrepareForm($form, $data) {
+    $app = JFactory::getApplication();
+    $option = $app->input->get('option');
+    $theadmincomponentsfiles = explode(",", $this->params->get('theadmincomponentsfiles'));
+    $theadmincomponents = explode(",", $this->params->get('theadmincomponents'));
+    $thesitecomponentsfiles = explode(",", $this->params->get('thesitecomponentsfiles'));
+    $thesitecomponents = explode(",", $this->params->get('thesitecomponents'));
+    $pieces = explode(",", $this->params->get('filenames'));
+    $themodules = explode(",", $this->params->get('themodules'));
+    $db = JFactory::getDBO();
+    $query = 'SELECT module' . ' FROM #__modules' . ' WHERE id = ' . $db->Quote($app->input->get("id"));
+    $db->setQuery($query);
+    $module = $db->loadResult();
 //Load the Site Default Template
-
-$dbtemplate    = JFactory::getDBO();
-
-$querytemplate = "SELECT template FROM #__template_styles WHERE client_id = 0 AND home = 1";
-
-$dbtemplate->setQuery($querytemplate);
-
-$defaulsitetemplate = $dbtemplate->loadResult();
-
-
-
-$dbadmintemplate    = JFactory::getDBO();
-
-$queryadmintemplate = "SELECT template FROM #__template_styles WHERE client_id = 1 AND home = 1";
-
-$dbadmintemplate->setQuery($queryadmintemplate);
-
-$defaultadmintemplate = $dbadmintemplate->loadResult();
-
- //For ADMIN COMPONENTS
-
-foreach($theadmincomponents as $adminkey=>$theadmincomponent){
-
-$theadmincomponent = trim($theadmincomponent);
-
-if ($option == $theadmincomponent){
-
-switch ($this->params->get('theadmincomponentsfilespaths')) {
-
-case 'system':
-
-JForm::addFormPath(JPATH_ADMINISTRATOR.'/templates/system/forms/'.$theadmincomponent. '/');
-
-break;
-
-case 'template_override':
-
-JForm::addFormPath(JPATH_ADMINISTRATOR . '/templates/' . $defaultadmintemplate . '/html/' . $theadmincomponent . '/forms/');
-
-break;
-
-case 'plugins':
-
-JForm::addFormPath(JPATH_PLUGINS . '/system/add_xml_fields/');
-
-break;
-
-}
-
-foreach ($theadmincomponentsfiles as $admink => $theadmincomponentsfile) {
-
-$form->loadFile($theadmincomponentsfiles[$adminkey], false);
-
-}
-
-}
-
-}
-
-
-
+    $dbtemplate = JFactory::getDBO();
+    $querytemplate = "SELECT template FROM #__template_styles WHERE client_id = 0 AND home = 1";
+    $dbtemplate->setQuery($querytemplate);
+    $defaulsitetemplate = $dbtemplate->loadResult();
+    $dbadmintemplate = JFactory::getDBO();
+    $queryadmintemplate = "SELECT template FROM #__template_styles WHERE client_id = 1 AND home = 1";
+    $dbadmintemplate->setQuery($queryadmintemplate);
+    $defaultadmintemplate = $dbadmintemplate->loadResult();
+//For ADMIN COMPONENTS
+    foreach ($theadmincomponents as $adminkey => $theadmincomponent) {
+      $theadmincomponent = trim($theadmincomponent);
+      if (in_array($option, $theadmincomponents)) {
+        switch ($this->params->get('theadmincomponentsfilespaths')) {
+          case 'system' :
+            JForm::addFormPath(JPATH_ADMINISTRATOR . '/templates/system/forms/' . $theadmincomponent . '/');
+            break;
+          case 'template_override' :
+            JForm::addFormPath(JPATH_ADMINISTRATOR . '/templates/' . $defaultadmintemplate . '/html/' . $theadmincomponent . '/forms/');
+            break;
+          case 'plugins' :
+            JForm::addFormPath(JPATH_PLUGINS . '/system/add_xml_fields/');
+            break;
+        }
+        foreach ($theadmincomponentsfiles as $admink => $theadmincomponentsfile) {
+          $form->loadFile($theadmincomponentsfiles[$adminkey], false);
+        }
+      }
+    }
 //For SITE COMPONENTS
-
-foreach($thesitecomponents as $sitekey=>$thesitecomponent){
-
-$thesitecomponent = trim($thesitecomponent);
-
-if ($option == $thesitecomponent){
-
-switch ($this->params->get('thesitecomponentsfilespaths')) {
-
-case 'system':
-
-JForm::addFormPath(JPATH_SITE.'/templates/system/forms/'.$thesitecomponent. '/');
-
-break;
-
-case 'template_override':
-
-JForm::addFormPath(JPATH_SITE . '/templates/' . $defaultsitetemplate . '/html/' . $thesitecomponent . '/forms/');
-
-break;
-
-case 'plugins':
-
-JForm::addFormPath(JPATH_PLUGINS . '/system/add_xml_fields/');
-
-break;
-
-}
-
-foreach ($thesitecomponentsfiles as $sitek => $thesitecomponentsfile) {
-
-$form->loadFile($thesitecomponentsfiles[$sitekey], false);
-
-}
-
-}
-
-}
-
-
-
+    foreach ($thesitecomponents as $sitekey => $thesitecomponent) {
+      $thesitecomponent = trim($thesitecomponent);
+      if (in_array($option, $thesitecomponents)) {
+        switch ($this->params->get('thesitecomponentsfilespaths')) {
+          case 'system' :
+            JForm::addFormPath(JPATH_SITE . '/templates/system/forms/' . $thesitecomponent . '/');
+            break;
+          case 'template_override' :
+            JForm::addFormPath(JPATH_SITE . '/templates/' . $defaultsitetemplate . '/html/' . $thesitecomponent . '/forms/');
+            break;
+          case 'plugins' :
+            JForm::addFormPath(JPATH_PLUGINS . '/system/add_xml_fields/');
+            break;
+        }
+        foreach ($thesitecomponentsfiles as $sitek => $thesitecomponentsfile) {
+          $form->loadFile($thesitecomponentsfiles[$sitekey], false);
+        }
+      }
+    }
 // For Site Modules
-
-foreach($themodules as $key=>$themodule){
-
-$themodule = trim($themodule);
-
-if ($module == $themodule){
-
-switch ($this->params->get('thepath')) {
-
-case 'template_override':
-
-JForm::addFormPath(JPATH_SITE . '/templates/' . $defaulsitetemplate . '/html/' . $themodule . '/');
-
-break;
-
-case 'plugins':
-
-JForm::addFormPath(JPATH_PLUGINS . '/system/add_xml_fields/');
-
-break;
-
+    foreach ($themodules as $key => $themodule) {
+      $themodule = trim($themodule);
+      if (in_array($module, $themodules)) {
+        switch ($this->params->get('thepath')) {
+          case 'template_override' :
+            JForm::addFormPath(JPATH_SITE . '/templates/' . $defaulsitetemplate . '/html/' . $themodule . '/');
+            break;
+          case 'plugins' :
+            JForm::addFormPath(JPATH_PLUGINS . '/system/add_xml_fields/');
+            break;
+        }
+        foreach ($pieces as $k => $piece) {
+          $form->loadFile($pieces[$key], false);
+        }
+      }
+    }
+  }
 }
-
-foreach ($pieces as $k => $piece) {
-
-$form->loadFile($pieces[$key], false);
-
-}
-
-}
-
-}
-
-}
-
-}
-
-
-
